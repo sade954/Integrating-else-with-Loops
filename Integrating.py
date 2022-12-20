a97771b1a1a2f2b286a5380f16bf48ba207f6313
Candace Hollinger <csig1985@gmail.com>
2022-12-20 04:53:40 +0000

    updating integrating

---
+++ Integrating.py
@@ -0,0 +1,22 @@
+for i in [1, 2, 3, 4 5]:
+    print(i)
+else:
+    print("For loop completed")
+Output = 
+1
+2
+3
+4
+5
+For loop completed
+
+#Using 'for' loops and the 'if' statement, along with 'break':
+colors = ['red', 'pink' 'blue', 'orange', 'green']
+for color in colors:
+    if color == 'Orange':
+        print('Orange is in the list')
+        break
+else:
+    print("Orange is not in the list")
+Output = 
+Orange is in the list
